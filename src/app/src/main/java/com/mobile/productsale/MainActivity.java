e0239cfb54572ec3dbcdbd1beaa003a27135950b
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-10-31 17:34:04 +0700

    add view register field

--- src/app/src/main/java/com/mobile/productsale/MainActivity.java
+++ src/app/src/main/java/com/mobile/productsale/MainActivity.java
@@ -29,16 +29,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView1 = findViewById(R.id.facebookLogo);
         ImageView imageView2 = findViewById(R.id.googleLogo);
 
-        String fb = "https://freepnglogo.com/images/all_img/facebook-circle-logo-png.png";
-        String gg = "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png";
-
-        Glide.with(this)
-                .load(fb)
-                .into(imageView1);
-
-        Glide.with(this)
-                .load(gg)
-                .into(imageView2);
+        IconLoader.loadImage(this, IconLoader.fb, imageView1);
+        IconLoader.loadImage(this, IconLoader.gg, imageView2);
 
         TextView register = findViewById(R.id.registerRedirect);
 
--- src/app/src/main/java/com/mobile/productsale/RegisterActivity.java
+++ src/app/src/main/java/com/mobile/productsale/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.mobile.productsale;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +20,42 @@ public class RegisterActivity extends AppCompatActivity {
 
         setContentView(R.layout.register);
 
-        TextView backToLogin = findViewById(R.id.backToLogin);
-        TextView returnToLogin = findViewById(R.id.loginRedirect);
+        ImageView fb = findViewById(R.id.facebookLogo);
+        ImageView gg = findViewById(R.id.googleLogo);
 
-        backToLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-            startActivity(intent);
+
+        IconLoader.loadImage(this, IconLoader.fb, fb);
+        IconLoader.loadImage(this, IconLoader.gg, gg);
+
+//      get animation
+        Animation toRight = AnimationUtils.loadAnimation(RegisterActivity.this, android.R.anim.slide_out_right);
+        Animation toLeft = AnimationUtils.loadAnimation(RegisterActivity.this, android.R.anim.slide_in_left);
+
+        ViewSwitcher registerSwitcher = findViewById(R.id.continueRegister);
+        TextView previousPage = findViewById(R.id.backToLogin);
+        TextView returnToLogin = findViewById(R.id.loginRedirect);
+        Button register = findViewById(R.id.registerBtn);
+
+//        set animation
+//        returnToLogin.setAnimation(toLeft);
+
+        previousPage.setOnClickListener(v -> {
+            if (registerSwitcher.getDisplayedChild() == 1) {
+                registerSwitcher.setInAnimation(toLeft);
+                registerSwitcher.showPrevious();
+            } else {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
 
         returnToLogin.setOnClickListener(v -> {
             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
             startActivity(intent);
+        });
+
+        register.setOnClickListener(v -> {
+            registerSwitcher.showNext();
         });
 
     }
